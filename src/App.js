7640bdd132d30789e6aec7a1c478fa269b2fbde9
adnankabir122 <adnankabir122@gmail.com>
2022-12-31 02:34:37 +0600

    pushes

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ import EmployerProfile from "./Components/EmployerProfile/EmployerProfile";
 const App = () => {
   return (
     <div>
-      <BrowserRouter>
+      <BrowserRouter basename={process.env.PUBLIC_URL} >
         <Routes>
           <Route path="/" element={<Home/>} />
           <Route path="home" element={<Home/>} />
